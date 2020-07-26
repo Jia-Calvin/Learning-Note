add90ec7060c8be7b0bf24046a3c6e636054cb4a
chenjunjia.calvin <chenjunjia.calvin@bytedance.com>
2020-07-26 13:02:38 +0800

    New Life

--- Twemproxy/src/buildinfo/buildinfo.c
+++
@@ -1,3 +0,0 @@
-const char *get_buildinfo() {
-    return BUILDINFO;
-}
--- Twemproxy/src/debug.sh
+++
@@ -1,3 +0,0 @@
-# ./nutcracker -c /opt/tiger/kv/twemproxy/conf/nutcracker_test.yml -v 11 -M /opt/tiger/kv/twemproxy/test/manage.sock -o /opt/tiger/kv/twemproxy/test/manage.log
-pid=`ps -fe | grep tw| grep -v grep| awk '{print $2}'`
-gdb nutcracker $pid
