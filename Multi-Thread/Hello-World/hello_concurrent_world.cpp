de5eaf512a7b474e58ca6665564185b94dd485f2
chenjunjia.calvin <616198312@qq.com>
2022-08-10 00:15:28 +0800

    feat: add hello concurrent world

---
+++ Multi-Thread/Hello-World/hello_concurrent_world.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+
+void helloLisa()
+{
+    std::cout << "HELLO LISA!" << std::endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    std::vector<std::thread> threads;
+    for (int i = 0; i < 100; i++)
+    {
+        auto t = std::thread(helloLisa);
+        threads.push_back(std::move(t));
+    }
+
+    for (int i = 0; i < threads.size(); i++)
+    {
+        threads[i].join();
+    }
+
+    return 0;
+}
