c9cd29698c1ecc962c9987ec89ad9e44632d2fa1
chenjunjia.calvin <chenjunjia.calvin@bytedance.com>
2023-03-11 19:41:41 +0800

    fix: refactored the entire directory structure

--- Algorithm/LeetCode/test.cpp
+++ Algorithm/LeetCode/test.cpp
@@ -46,6 +46,8 @@ class MyPrinter {
 protected:
     MyPrinter(/* args */) = default;
     ~MyPrinter() = default;
+
+
     // 中序遍历，递归方法
     static void printTreeCore(TreeNode* root) {
         if (root == nullptr) {
@@ -98,14 +100,90 @@ public:
         }
         std::cout << std::endl;
     }
+
+    template <class K, class V, class C>
+    static void printMap(std::map<K, V, C>& maps) {
+        for (auto&& kv : maps) {
+            std::cout << kv.first << ", [";
+            for (auto&& ele : kv.second) {
+                std::cout << ele << ", ";
+            }
+            std::cout << "]" << std::endl;
+        }
+        std::cout << std::endl;
+    }
 };
 
 class Solution {
 public:
+    int leastInterval(vector<char>& tasks, int n) {
+        if (n == 0) {
+            return tasks.size();
+        }
+        std::unordered_map<char, int> keyToFre;
+        std::map<int, std::list<char>, std::greater<int>> freToKeyList;
+        for (int i = 0; i < tasks.size(); i++) {
+            keyToFre[tasks[i]]++;
+        }
+        for (auto iter = keyToFre.begin(); iter != keyToFre.end(); iter++) {
+            freToKeyList[iter->second].push_back(iter->first);
+        }
+        int left = 0, right = -1;
+        vector<char> rebuildTasks;
+        std::unordered_map<char, bool> usedTask;
+        while (!freToKeyList.empty()) {
+            MyPrinter::printMap(freToKeyList);
+            bool getEle = false;
+            for (auto iterMap = freToKeyList.begin(); iterMap != freToKeyList.end(); iterMap++) {
+                for (auto iterList = iterMap->second.begin(); iterList != iterMap->second.end();
+                     iterList++) {
+                    char element = *iterList;
+                    if (!usedTask[element]) {
+                        rebuildTasks.push_back(element);
+                        MyPrinter::printVector(rebuildTasks);
+                        usedTask[element] = true;
+                        if (iterMap->first != 1) {
+                            freToKeyList[iterMap->first - 1].push_back(element);
+                        }
+
+                        iterMap->second.erase(iterList);
+                        if (iterMap->second.empty()) {
+                            freToKeyList.erase(iterMap);
+                        }
+
+                        right++;
+                        if (right - left + 1 > n) {
+                            usedTask[rebuildTasks[left]] = false;
+                            left++;
+                        }
+                        getEle = true;
+                        break;
+                    }
+                }
+                if (getEle) {
+                    break;
+                }
+            }
+            if (!getEle) {
+                rebuildTasks.push_back('0');
+                right++;
+                if (right - left + 1 > n) {
+                    usedTask[rebuildTasks[left]] = false;
+                    left++;
+                }
+            }
+        }
+        return rebuildTasks.size();
+    }
 };
 
 int main(int argc, char const* argv[]) {
     Solution s;
+    vector<char> nums = {'A', 'A', 'A', 'B', 'B', 'B'};
+    cout << s.leastInterval(nums, 50) << endl;
+
+    // nums = {'A', 'A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+    // cout << s.leastInterval(nums, 2) << endl;
 
     return 0;
 }
